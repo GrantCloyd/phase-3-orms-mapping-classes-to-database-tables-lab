270cf13b37943ef018a228addd9c03aacaa196ae
Grant Cloyd <dgcloyd@gmail.com>
2021-07-03 14:44:47 -0500

    commit tree 54a71951413ab72899e61aec691e6e3702c69f89

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,46 @@
 class Student
+  attr_reader :id
+  attr_accessor :name, :grade
+  def initialize(name, grade, id=nil)
+  @name = name
+  @grade = grade
+  @id = id
+  end
+
+  def self.create_table  
+  sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+      
+    );
+     SQL
+     DB[:conn].execute(sql)
+  end
+
+  def self.drop_table 
+  sql = "
+  DROP TABLE IF EXISTS students;
+  "
+  DB[:conn].execute(sql)
+  end
+
+  def save  
+  sql = <<-SQL 
+  INSERT INTO students (name, grade) VALUES ("#{self.name}", "#{self.grade}")
+  SQL
+  DB[:conn].execute(sql)
+
+  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM Students").first.first
+  end
+
+  def self.create(hashtributes = {})
+   #hashtributes.each {|key, value| self.send(("#{key}="), value)}
+   student = Student.new(hashtributes[:name], hashtributes[:grade])
+   student.save
+   student
+  end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
